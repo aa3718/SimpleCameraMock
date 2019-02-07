ce7713ca6ba19d64ebcd6894b80f8d33bdcf68fd
aa3718 <aa3718@ic.ac.uk>
2019-02-07 11:18:20 +0000

    Marthe and Allegra : Working test that camera.powerOn() triggers sensor.powerUp()

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -1,17 +1,34 @@
 package ic.doc.camera;
 
-public class Camera {
+public class Camera implements WriteListener {
+
+  private boolean power;
+  private Sensor sensor;
+  private MemoryCard memoryCard;
+
+  public Camera(Sensor sensorCnstr, MemoryCard memoryCardCnstr) {
+    power = false;
+    sensor = sensorCnstr;
+    memoryCard = memoryCardCnstr;
+  }
 
   public void pressShutter() {
     // not implemented
   }
 
   public void powerOn() {
+    power = true;
+    sensor.powerUp();
     // not implemented
   }
 
   public void powerOff() {
     // not implemented
+  }
+
+  @java.lang.Override
+  public void writeComplete() {
+
   }
 }
 
